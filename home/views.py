91f8a084e166a0b6623093b87bfb6d8166a6637c
AnindoTonoy <anindo350@gmail.com>
2020-08-29 22:26:01 +0600

    :tada: Initial commit

---
+++ home/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout, login
+
+# Super user
+# Username: admin
+# password: admin
+
+# User login info for test
+# username: anindo
+# password for tesr user Anindo is: Anindo$$$0000
+
+# Create your views here.
+def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
+
+    return render(request, 'index.html')
+
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        
+        # Check user has entered correct credentials
+        user = authenticate(username= username, password= password)
+        
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request, user)
+            return redirect("/")
+        
+        else:
+            # No backend authenticated the credentials
+            return render(request, 'login.html')
+
+    return render(request, 'login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
